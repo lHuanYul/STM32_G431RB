0e1b5fd7724e49e7766e26968b97d0a0593386d5
Yuru-saiko <A21507375123@gmail.com>
2025-12-11 17:52:03 +0800

    up

--- sensor/Core/Src/main/main.c
+++ sensor/Core/Src/main/main.c
@@ -8,30 +8,24 @@ inline void INIT_OWN(void)
     INIT_OWN_TIM();
 }
 
-int text_exit = 0;
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-    text_exit++;
     if (GPIO_Pin == us_sensor_head.const_h.echo_GPIO_Pin_x) // 6
     {
         us_sensor_echo();
     }
 }
 
-int text_over = 0;
 void HAL_TIM_PeriodElapsedCallback_OWN(TIM_HandleTypeDef *htim)
 {
-    text_over++;
     if (htim == us_sensor_head.const_h.htimx)
     {
         us_sensor_overflow();
     }
 }
 
-int text_tim = 0;
 void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
 {
-    text_tim++;
     if (
         htim == us_sensor_head.const_h.htimx &&
         htim->Channel == us_sensor_head.const_h.HAL_TIM_ACTIVE_CHANNEL_x
